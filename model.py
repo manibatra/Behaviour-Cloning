9732a8302359978647c8f500270ee74302c83791
Mani Batra <manibatra2002@gmail.com>
2018-06-24 11:16:52 +0000

    reading image using image names

--- model.py
+++ model.py
@@ -1,7 +1,12 @@
 import os
 import csv
 
+import matplotlib.image as mpimg
+import numpy as np
+from PIL import Image
+
 image_labels = []
+image_names = []
 image_data = []
 
 with open('./data/driving_log.csv') as csvfile:
@@ -9,11 +14,15 @@ with open('./data/driving_log.csv') as csvfile:
 	next(reader)
 	for line in reader:
 		for i in range(3):
-			image_data.append('data/' + line[i])
+			image_names.append('./data/' + line[i].strip())
 		steering_value = float(line[3])
 		image_labels.append(steering_value)
 		image_labels.append(steering_value + 0.2)
-		image_labels.append(steering_value - 0.2)
+		image_labels.append(steering_value - 0.2) 
 
+print(image_names[0])
+#testing for a few images
+for i in range(50):
+	image_data.append(np.array(Image.open(image_names[i])))
 
-print(len(image_labels), len(image_data))
+print(image_data[0].shape)
