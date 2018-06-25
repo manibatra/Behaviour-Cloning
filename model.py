9bbe90b09dabcdb1cfa13d06f987c9954a52c709
Mani Batra <manibatra2002@gmail.com>
2018-06-25 03:40:18 +0000

    using the comma.ai model

--- model.py
+++ model.py
@@ -3,7 +3,10 @@ import csv
 
 import matplotlib.image as mpimg
 import numpy as np
-from PIL import Image
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, ELU, Dropout
+from keras.layers.convolutional import Convolution2D
 
 image_labels = []
 image_names = []
@@ -17,12 +20,35 @@ with open('./data/driving_log.csv') as csvfile:
 			image_names.append('./data/' + line[i].strip())
 		steering_value = float(line[3])
 		image_labels.append(steering_value)
-		image_labels.append(steering_value + 0.2)
-		image_labels.append(steering_value - 0.2) 
+		image_labels.append(steering_value + 0.1)
+		image_labels.append(steering_value - 0.1) 
 
 print(image_names[0])
 #testing for a few images
-for i in range(50):
-	image_data.append(np.array(Image.open(image_names[i])))
+for i in range(len(image_names)):
+	image_data.append(mpimg.imread(image_names[i]))
 
-print(image_data[0].shape)
+image_data = np.array(image_data)
+image_labels = np.array(image_labels)
+shape = image_data[0].shape
+model = Sequential()
+model.add(Lambda(lambda x: x/127.5 - 1., input_shape=shape, output_shape=shape))
+model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
+model.add(ELU())
+model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
+model.add(ELU())
+model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
+model.add(Flatten())
+model.add(Dropout(.2))
+model.add(ELU())
+model.add(Dense(512))
+model.add(Dropout(.5))
+model.add(ELU())
+model.add(Dense(1))
+
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(image_data, image_labels, validation_split=0.2, shuffle=True, nb_epoch=10)
+
+model.save('model.h5')
+
