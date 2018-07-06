1fc719e6f5d6c9b268ab0384ab8c95c672ba7717
Mani Batra <manibatra2002@gmail.com>
2018-07-06 17:31:07 +1000

    cleaned the code

--- model.py
+++ model.py
@@ -20,7 +20,7 @@ image_labels = []
 image_names = []
 image_data = []
 
-paths = ['./examples/data/', './track_2_data/', './patch/', './']
+paths = [] //add paths to data here
 
 for path in paths:
 	with open(path + 'driving_log.csv') as csvfile:
@@ -37,11 +37,11 @@ for path in paths:
 train_samples, validation_samples, train_labels, validation_labels  = train_test_split(image_names, image_labels, test_size=0.2)
 
 shape = np.expand_dims(cv2.split(cv2.cvtColor(mpimg.imread(image_names[0]), cv2.COLOR_RGB2YUV))[0],  axis=3).shape
-#shape = mpimg.imread(image_names[0]).shape
 
-
+//callback for early stopping
 stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=2, verbose=1)
 
+//generator to process images in batches
 def generator(samples, labels, batch_size=32):
 	num_samples = len(samples)
 	while 1:
@@ -56,13 +56,13 @@ def generator(samples, labels, batch_size=32):
 			for i in range(len(batch_samples)):
 				image = mpimg.imread(batch_samples[i])
 				angle = batch_angles[i]
+				//splitting the image into YUV color channels
 				c1, c2, c3 = cv2.split(cv2.cvtColor(image, cv2.COLOR_RGB2YUV))
 				image = np.expand_dims(c1, axis=3)
 				images.append(image)
 				angles.append(angle)
-				shape = image.shape
+				//flipping the image and the steering angle
 				image = np.expand_dims(cv2.flip(image, 0), axis=3)
-			#	image = cv2.flip(image, 0)
 				angle *= -1
 				#print("image after flip", image.shape)
 				images.append(image)
@@ -106,17 +106,12 @@ def get_nvidia_model(regularizer):
 	model.add(Lambda(lambda x: x/127.5 - 1.))
 	model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu', W_regularizer=regularizer))
 	model.add(BatchNormalization())
-#	model.add(MaxPooling2D())
 	model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu', W_regularizer=regularizer))
 	model.add(BatchNormalization())
-#	model.add(MaxPooling2D())
 	model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu', W_regularizer=regularizer))
 	model.add(BatchNormalization())
-#	model.add(MaxPooling2D())
 	model.add(Convolution2D(64, 3, 3, activation='relu', W_regularizer=regularizer))
 	model.add(BatchNormalization())
-#	model.add(MaxPooling2D())
-#	model.add(Convolution2D(64, 3, 3, activation='relu'))
 	model.add(Flatten())
 	model.add(Dense(100))
 	model.add(BatchNormalization())
@@ -128,7 +123,6 @@ def get_nvidia_model(regularizer):
 	#model.add(Dropout(0.2))
 	model.add(Dense(10))
 	model.add(Activation('relu'))
-	#model.add(Dropout(0.5))
 	model.add(Dense(1))
 	return model
 
@@ -136,7 +130,6 @@ model = get_nvidia_model(None)
 #model = get_comma_ai_model()
 adam = optimizers.adam(lr=0.01)
 model.compile(loss='mse', optimizer=adam)
-#model = load_model("model_nvidia.h5")
 model.fit_generator(train_generator, samples_per_epoch=len(train_samples) * 2, validation_data=validation_generator, nb_val_samples=len(validation_samples) * 2, nb_epoch=25, callbacks=[stopping])
-model.save('model_nvidia_track2.h5')
+model.save('saved_model.h5')
 
